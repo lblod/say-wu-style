eaa3c2cd689972d0e8ea637b57958a77f7c3a440
Aad Versteden <madnificent@gmail.com>
2019-08-05 14:19:35 +0200

    First abstraction for list
    Pair programming with @mietcls is awesome!

---
+++ addon/components/say/hint.js
@@ -0,0 +1,6 @@
+import Component from '@ember/component';
+import layout from '../../templates/components/say/hint';
+
+export default Component.extend({
+  layout
+});
---
+++ addon/components/say/hint/list/list-item.js
@@ -0,0 +1,6 @@
+import Component from '@ember/component';
+import layout from '../../../../templates/components/say/hint/list/list-item';
+
+export default Component.extend({
+  layout
+});
